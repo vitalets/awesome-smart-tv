6c4490f18611ce7e9cdb9683ac7b6c7717be94fa
Oscar Gomez <42280574+oscarg933@users.noreply.github.com>
2018-12-07 20:37:43 -0700

    Create Sharepoint.pnp.php

---
+++ Sharepoint.pnp.php
@@ -0,0 +1,82 @@
+Skip to content
+ 
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ @oscarg933 Sign out
+You are over your private repository plan limit (4 of 0). Please upgrade your plan, make private repositories public, or remove private repositories so that you are within your plan limit.
+Your private repositories have been locked until this is resolved. Thanks for understanding. You can contact support with any questions.
+0
+0 3,017 oscarg933/PnP
+forked from SharePoint/PnP
+ Code  Issues 0  Pull requests 0  Projects 0  Wiki  Insights  Settings
+SharePoint / Office 365 Developer Patterns and Practices
+ 4,730 commits
+ 2 branches
+ 0 releases
+ 139 contributors
+ View license
+ C# 62.3%	 JavaScript 13.6%	 CSS 11.1%	 HTML 6.1%	 ASP 3.4%	 TypeScript 2.7%	 Other 0.8%
+ Pull request   Compare This branch is 2 commits ahead of SharePoint:master.
+@oscarg933
+oscarg933 Update issue templates  …
+Latest commit a95e065  a minute ago
+Type	Name	Latest commit message	Commit time
+.github	Update issue templates	a minute ago
+Components	enabled taxonomypicker synomym matching for search (SharePoint#1774)	7 months ago
+Samples	updated to latest nuget	16 hours ago
+Solutions	updates	3 months ago
+.gitattributes	Reset to original value	4 years ago
+.gitignore	Updated Batch Update sample based on latest with the API.	3 years ago
+CHANGELOG.md	Prep changelog for November release	29 days ago
+LICENSE	Update LICENSE	2 years ago
+README.md	December 2018 release	16 hours ago
+_config.yml	Set theme jekyll-theme-tactile	2 minutes ago
+readme-template.md	Small changes + documentation changes, including document MD template	4 years ago
+ README.md
+Patterns and Practices
+This is the main repository for the community driven SharePoint / Office Dev Patterns and Practices (PnP) initiative. If you are looking for latest news around PnP or related topics, please have a look on our one pager at http://aka.ms/sppnp.
+
+SharePoint Patterns and Practices logo
+
+PnP initiative has numerous GitHub repositories, so that you can more easily find what's relevant for you depending on your interest. Easiest way to follow up on latest changes is our landing page at http://aka.ms/OfficeDevPnP. We also recommend you to use the PnP sample and solution search tool at dev.office.com for finding relevant content for you.
+
+PnP at dev.office.com - Welcome page with sample and guidance search
+SharePoint Dev Group - Questions and feedback
+PnP at MSDN
+PnP videos at YouTube
+PnP at Docs.com - Docs.com
+PnP Sites Core Component - GitHub repository
+PnP Core Component (JavaScript) - GitHub repository
+PnP PowerShell - GitHub repository
+PnP Partner Pack - Reusable starter kit for typical enterprise requirements
+PnP Guidance - GitHub repository
+PnP Office-Addins - GitHub repository
+PnP Tools - GitHub repository
+PnP Transformation - GitHub repository
+PnP Provisioning Schema - GitHub repository
+You could be also interested on seeing all PnP resources in one PowerPoint slide.
+
+A list of links to the various contact points for the Office 365 Developer Patterns and Practices group and content.
+
+Notice. There are different kind of techniques shown in the PnP guidance for apps/add-ins targetted to store and also for apps targeted to be deployed or created more as enterprise developmented apps/add-ins. Any app/add-in which requires higher than Manage permission to the host web cannot be distributed using SharePoint store.*
+
+"Sharing is caring"
+This project has adopted the Microsoft Open Source Code of Conduct. For more information see the Code of Conduct FAQ or contact opencode@microsoft.com with any additional questions or comments.
+
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Press h to open a hovercard with more details.
